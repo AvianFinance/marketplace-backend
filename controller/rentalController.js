01e282cf0c9b5880006d6f64853e0dbf4b4a003e
Roshinie Jayasundara <roshinie.18@cse.mrt.ac.lk>
2023-02-19 19:09:49 +0530

    Get rental by collection

--- controller/rentalController.js
+++ controller/rentalController.js
@@ -10,21 +10,19 @@ const getRentalCollections = async (req, res) => {
   let collection = await db.collection(collectionName);
 
   const tx = await m_contract.getRListedAdddresses() // Gives all the token addresses listed for renting
-  console.log(tx)
   // tx = ["0xA5e80F4980878b7C2c23D6fA002358A47d0060a3","0x4909493F604AB882327ca880ad5B330e2B3C43C1"]
   output = []
  
   for (i in tx) {
       let query = { _id: tx[i]};
       let result = await collection.findOne(query);
-      console.log(result)
+
       let tokensList = await db.collection("nft_details").find({coll_addr: tx[i]}).toArray();
-      console.log(tokensList)
+
       if (tokensList.length > 0) {
           result.count = tokensList.length
           let uriList = []
           for (token in tokensList) {
-            // console.log(tokenList[token])
             uriList.push(tokensList[token].uri)
           }
           if(uriList.length > 4) {
@@ -40,9 +38,11 @@ const getRentalCollections = async (req, res) => {
 // @desc Get the rentals in a collection
 // @route GET /api/rental/explore/:collectionID
 const getRentalCollectionTokens = async (req, res) => {
-  const m_contract = mplace_contract.createABI();
+    const m_contract = mplace_contract.createABI();
     const token_address = req.params.collectionId
+
     const tx = await m_contract.getRListedAdddressTokens(token_address);
+    console.log(tx)
 
     const db = dbo.getDb();
     let collection = await db.collection("nft_details");
@@ -50,8 +50,14 @@ const getRentalCollectionTokens = async (req, res) => {
     let output = []
     for (i in tx) {
       let query = { coll_addr : token_address, token_id : tx[i].toNumber()}
-      let result = await collection.find(query).toArray();
-      output.push(result[0])
+      let result = (await collection.find(query).toArray())[0];
+      // console.log(result)
+      const listing= await m_contract.getARListing(token_address, tx[i].toNumber() );
+      result.pricePerDay = listing.pricePerDay
+      result.startDateUNIX = listing.startDateUNIX
+      result.endDateUNIX = listing.endDateUNIX
+      result.expires = listing.expires
+      output.push(result)
     }
     res.send(output).status(200);
 }
