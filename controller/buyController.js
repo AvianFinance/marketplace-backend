e0be6585a12d337115c2605e66a159768d91e0b0
Roshinie Jayasundara <roshinie.18@cse.mrt.ac.lk>
2023-02-19 15:23:14 +0530

    Explore buy NFTs endpoint

--- controller/buyController.js
+++ controller/buyController.js
@@ -1,12 +1,38 @@
-const db = require('../database/conn')
+const dbo = require('../database/conn')
 const mplace_contract = require('../config/contract')
+const collectionName = "collections"
 
 // @desc Get the rentals
 // @route GET /api/buy/explore
 const getBuyCollections = async (req, res) => {
-  const m_contract = mplace_contract.createABI();
-  const tx = await m_contract.getSellListings() // Gives all the token addresses listed for renting
-  res.send(tx).status(200);
+    const m_contract = mplace_contract.createABI();
+    const db = dbo.getDb();
+    let collection = await db.collection(collectionName);
+
+    const tx = await m_contract.getSListedAdddresses() // Gives all the token addresses listed for renting
+    console.log(tx)
+    // tx = ["0xA5e80F4980878b7C2c23D6fA002358A47d0060a3","0x4909493F604AB882327ca880ad5B330e2B3C43C1"]
+    output = []
+   
+    for (i in tx) {
+        let query = { _id: tx[i]};
+        let result = await collection.findOne(query);
+        
+        if (result.tokens.length > 0) {
+            let tokenList = result.tokens
+            let uriList = []
+            for (token in tokenList) {
+              // console.log(tokenList[token])
+              uriList.push(tokenList[token].uri)
+            }
+            if(uriList.length > 4) {
+              uriList = uriList.slice(0, 4)
+            }
+            result.tokens = uriList
+        }
+        output.push(result);
+    }
+    res.send(output).status(200)
 }
 
 // @desc Get the rentals in a collection
