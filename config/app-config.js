ffe37b8c636f4dedf4495b20d6d5530bf8e800dc
Roshinie Jayasundara <roshinie.18@cse.mrt.ac.lk>
2023-03-16 13:59:01 +0530

    FIX: Update configurations

--- config/app-config.js
+++ config/app-config.js
@@ -5,5 +5,6 @@ module.exports = {
     rime_token: process.env.RIME_TOKEN,
     rime_rent: process.env.RIME_RENT,
     amplace_token: process.env.AMPLACE_TOKEN,
-    db_connection: process.env.MONGODB_CONNECTION_STRING
+    db_connection: process.env.MONGODB_CONNECTION_STRING,
+    insmplace_token: process.env.INSMPLACE_TOKEN 
 }
