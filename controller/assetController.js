6ed6da7adacd98994ca85d8c7fc1c451733078f1
Roshinie Jayasundara <roshinie.18@cse.mrt.ac.lk>
2023-03-23 13:28:57 +0530

    REFAC: Modify get one nft details route

--- controller/assetController.js
+++ controller/assetController.js
@@ -1,13 +1,50 @@
 const dbo = require("../database/conn");
+const { getUserByAddress } = require('./userController')
+const logger = require("../utils/logger")
 
-const getOneNft = async (req, res) => {
-    const db = dbo.getDb();
-    let query = { coll_addr : req.params.collectionId , token_id : parseInt(req.params.tokenId) } ;
-    let result = await db.collection("nft_details").findOne(query);
-    if (!result) res.send("Not found").status(404);
-    else res.send(result).status(200);
+const getOneNft = async (req, res, next) => {
+    try {
+        const db = dbo.getDb();
+        let query = { coll_addr: req.params.collectionId, token_id: parseInt(req.params.tokenId) };
+        let nft_data = await db.collection("nft_details").findOne(query);
+        // console.log(nft_data)
+        
+        if (nft_data) {
+            nft_data.minterName = (await getUserByAddress(nft_data.minter)).name || "undefined"
+            nft_data.ownerName = (await getUserByAddress(nft_data.owner)).name || "undefined"
+            nft_data.userName = (await getUserByAddress(nft_data.user)).name || "undefined"
+            // console.log(nft_data)
+
+            let collection_data = await db.collection("collections").findOne({ _id: req.params.collectionId });
+
+            let listings = [];
+            if(nft_data.sell_listed_status){
+                const s_query = { nftAddress: req.params.collectionId, tokenId: parseInt(req.params.tokenId), listed_status: true, sold_status: false };
+                let sell_listed = await db.collection("sell_listings").findOne(s_query);
+                if ( sell_listed ) listings.push(sell_listed) ;
+            }
+            if(nft_data.rent_listed_status){
+                const r_query = { nftContract: req.params.collectionId, tokenId: parseInt(req.params.tokenId), listed_status : true, rent_status: false};
+                let rent_listed = await db.collection("rental_listings").findOne(r_query);
+                if (rent_listed)  listings.push(rent_listed) ;
+            }
+            if(nft_data.inst_listed_status){
+                const i_query = { nftContract: req.params.collectionId, tokenId: parseInt(req.params.tokenId), listed_status : true, inst_status: false};
+                let inst_listed = await db.collection("inst_listings").findOne(i_query);
+                if (inst_listed) listings.push(inst_listed);
+            }
+            let output = { nft: nft_data, collection: collection_data, listing : listings }
+            res.send(output).status(200);
+        } else {
+            res.send({ error: true, "message": "Invalid collection/token ID" })
+        }
+
+    } catch (err) {
+        logger.error(err);
+        next({ status: 500, message: err.message })
+    }
 }
 
-module.exports = { 
+module.exports = {
     getOneNft
 }
