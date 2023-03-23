53377038cb3ba5aa8d6d2b47af8f4a5f50a29267
Roshinie Jayasundara <roshinie.18@cse.mrt.ac.lk>
2023-03-23 16:19:22 +0530

    REFAC: Add collection details to explore data

--- controller/rentalController.js
+++ controller/rentalController.js
@@ -6,93 +6,103 @@ const logger = require('../utils/logger')
 
 // @desc Get the rentals
 // @route GET /api/rental/explore
-const getRentalCollections = async (req, res) => {
-  const m_contract = mplace_contract.createABI();
-  const instm_contract = mplace_contract.createInsABI();
-
-  const db = dbo.getDb();
-  let collection = await db.collection(collectionName);
-
-  const r_tx = await m_contract.getRListedAdddresses() // Gives all the token addresses listed for renting
-  logger.info(`Rental list ${r_tx}`)
-  const ins_tx = await instm_contract.getInsListedAdddresses() // Gives all the token addresses listed for installement renting
-  logger.info(`Installement Rental list ${ins_tx}`)
-
-  output = []
-  r_output = []
-  ins_output = []
-
-  for (i in r_tx) {
-    let query = { _id: r_tx[i] };
-    let result = await collection.findOne(query);
-    if (result) {
-      let user = await getUserByAddress(result.createdBy)
-      result.createdUserName = user.name
-      result.createdUserImage = user.profileImage
-      result.type = "UPRIGHT"
-
-      let tokensList = await db.collection("nft_details").find({ coll_addr: r_tx[i] }).toArray();
-
-      if (tokensList.length > 0) {
-        result.count = tokensList.length
-        let uriList = []
-        for (token in tokensList) {
-          uriList.push(tokensList[token].uri)
+const getRentalCollections = async (req, res, next) => {
+  try {
+    const m_contract = mplace_contract.createABI();
+    const instm_contract = mplace_contract.createInsABI();
+  
+    const db = dbo.getDb();
+    let collection = await db.collection(collectionName);
+  
+    const r_tx = await m_contract.getRListedAdddresses() // Gives all the token addresses listed for renting
+    logger.info(`Rental list ${r_tx}`)
+    const ins_tx = await instm_contract.getInsListedAdddresses() // Gives all the token addresses listed for installement renting
+    logger.info(`Installement Rental list ${ins_tx}`)
+  
+    output = []
+    r_output = []
+    ins_output = []
+  
+    for (i in r_tx) {
+      let query = { _id: r_tx[i] };
+      let result = await collection.findOne(query);
+      if (result) {
+        let user = await getUserByAddress(result.createdBy)
+        result.createdUserName = user.name
+        result.createdUserImage = user.profileImage
+        result.type = "UPRIGHT"
+  
+        let tokensList = await db.collection("nft_details").find({ coll_addr: r_tx[i] }).toArray();
+  
+        if (tokensList.length > 0) {
+          result.count = tokensList.length
+          let uriList = []
+          for (token in tokensList) {
+            uriList.push(tokensList[token].uri)
+          }
+          if (uriList.length > 4) {
+            uriList = uriList.slice(0, 4)
+          }
+          result.tokens = uriList
         }
-        if (uriList.length > 4) {
-          uriList = uriList.slice(0, 4)
-        }
-        result.tokens = uriList
+        r_output.push(result);
+      } else {
+        logger.info(`Details of Collection ${r_tx[i]} is not in the database`)
       }
-      r_output.push(result);
-    } else {
-      logger.info(`Details of Collection ${r_tx[i]} is not in the database`)
     }
-  }
-
-  for (i in ins_tx) {
-    let query = { _id: ins_tx[i] };
-    let result = await collection.findOne(query);
-    if (result) {
-      let user = await getUserByAddress(result.createdBy)
-      result.createdUserName = user.name
-      result.createdUserImage = user.profileImage
-      result.type = "INST"
-
-      let tokensList = await db.collection("nft_details").find({ coll_addr: ins_tx[i] }).toArray();
-
-      if (tokensList.length > 0) {
-        result.count = tokensList.length
-        let uriList = []
-        for (token in tokensList) {
-          uriList.push(tokensList[token].uri)
+  
+    for (i in ins_tx) {
+      let query = { _id: ins_tx[i] };
+      let result = await collection.findOne(query);
+      if (result) {
+        let user = await getUserByAddress(result.createdBy)
+        result.createdUserName = user.name
+        result.createdUserImage = user.profileImage
+        result.type = "INST"
+  
+        let tokensList = await db.collection("nft_details").find({ coll_addr: ins_tx[i] }).toArray();
+  
+        if (tokensList.length > 0) {
+          result.count = tokensList.length
+          let uriList = []
+          for (token in tokensList) {
+            uriList.push(tokensList[token].uri)
+          }
+          if (uriList.length > 4) {
+            uriList = uriList.slice(0, 4)
+          }
+          result.tokens = uriList
         }
-        if (uriList.length > 4) {
-          uriList = uriList.slice(0, 4)
-        }
-        result.tokens = uriList
+        ins_output.push(result);
+      } else {
+        logger.info(`Details of Collection ${ins_tx[i]} is not in the database`)
       }
-      ins_output.push(result);
-    } else {
-      logger.info(`Details of Collection ${ins_tx[i]} is not in the database`)
     }
+    output = { upright: r_output, inst: ins_output }
+    res.send(output).status(200)
+  } catch (err) {
+    logger.error(err);
+    next({ status: 500, message: err.message })
   }
-  output = { upright: r_output, inst: ins_output }
-  res.send(output).status(200)
+  
 }
 
 // @desc Get the rentals in a collection
 // @route GET /api/rental/explore/:collectionID
-const getRentalCollectionTokens = async (req, res) => {
-  const m_contract = mplace_contract.createABI();
-  const instm_contract = mplace_contract.createInsABI();
+const getRentalCollectionTokens = async (req, res, next) => {
 
-  const token_address = req.params.collectionId
   try {
+    const m_contract = mplace_contract.createABI();
+    const instm_contract = mplace_contract.createInsABI();
+  
+    const db = dbo.getDb();
+    
+    const token_address = req.params.collectionId
+
+    const collection_data = await db.collection("collections").findOne({ _id: token_address })
     const r_tx = await m_contract.getRListedAdddressTokens(token_address);
     const ins_tx = await instm_contract.getInsListedAdddressTokens(token_address);
 
-    const db = dbo.getDb();
     let collection = await db.collection("nft_details");
 
     output = []
@@ -131,11 +141,11 @@ const getRentalCollectionTokens = async (req, res) => {
         logger.info(`Details of token ${ins_tx[i]} is not in the database`)
       }
     }
-    output = { upright: r_output, inst: ins_output }
+    output = { collection: collection_data, upright: r_output, inst: ins_output }
     res.send(output).status(200);
   } catch (err) {
-    logger.error("Invalid Contract address")
-    res.status(500).send({ message: 'Invalid contract address!'});
+    logger.error(err);
+    next({ status: 500, message: err.message })
   }
 }
 
