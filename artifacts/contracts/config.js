51f3643dcf7456cf38fa4eefce915a37c03cdbea
Isuru Ariyarathne <61895498+IsuruAriyarathne@users.noreply.github.com>
2023-06-20 22:20:45 +0530

    Update contract

---
+++ artifacts/contracts/config.js
@@ -0,0 +1,29 @@
+// ins_impl_addr = "0xf413627E4BF31a4Be40dD50728f2E4131e17E0d6"
+
+// rent_impl_addr = "0x97a153A9d9a02bF6278aFAF0aF5009891D8C8B7f"
+
+// sell_impl_addr = "0xF4f4d8d956de6B3d68028c480706F2dBaAE34B40"
+
+sell_proxy_addr = "0xed778788773BeA7ea61635d33596A6dFc4a86900"
+rent_proxy_addr = "0xA5cDcBdeb08D4126D57c42A573Dc39958E0eCB19"
+installment_proxy_addr = "0xF2350c531812ED5352372673a83d451A7463C43b"
+rime_token   = "0x4909493F604AB882327ca880ad5B330e2B3C43C1"
+rime_rent = "0xA5e80F4980878b7C2c23D6fA002358A47d0060a3"
+
+//isuru
+// ins_impl_token = "0xcA34756bAbd1b49318955caF66D4acf993Ff70b1"
+// ins_proxy_token = "0xB388C5c0114AC0A05413B322386c9c8FE812Eb05"
+
+// rent_impl_token = "0x6D5525A975D3CF9A0Fe89abcF8D30aaf02E65434"
+// rent_proxy_token = "0x9bF19331CF09Eb6A693D78AeEb39A4E0C21113e1"
+
+// sell_impl_token = "0x3823f9B9ede8d22362a2D59A100aa99Fd615dB44"
+// sell_proxy_token = "0xCd54F936D1cE5cF9AffF5F56B4d3F3148bc114f9"
+
+module.exports = {
+    sell_proxy_addr,
+    rent_proxy_addr,
+    installment_proxy_addr,
+    rime_token,
+    rime_rent
+};
